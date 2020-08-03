d2c1c53c8e96d56d21f6c5b3c59d966fb0147bed
chenliang <860714835@qq.com>
2020-08-03 09:29:44 +0800

    222

--- demo01.py
+++ demo01.py
@@ -14,8 +14,25 @@
 # d.append(c)
 # print("三个数字之和:{}".format(a+b+c))
 
-e = [2,45,12,65,1231,0]
-p = e.index(65)
-print(p)
-e.sort(reverse = True)
+# e = [2,45,12,65,1231,0]
+# p = e.index(65)
+# print(p)
+# e.sort(reverse = True)
+# print(e)
+
+# student = {}
+# for i in range(0,3):
+#     key = input("请输入姓名: ")
+#     value = input("请输入成绩: ")
+#     student[key]=value
+# print(student)
+
+a =  ["zhangsan", "lixi", "wanger"]
+b = input("请输入3个密码用逗号隔开: ")
+c = b.split(",")
+e = {a[0]:c[0],a[1]:c[1],a[2]:c[2]}
 print(e)
+
+"""
+
+"""
---
+++ demo02.py
@@ -0,0 +1,9 @@
+a = [1,"diud",833,True,False,1,("qwdqw222",12),{}]
+print(a.index(833))
+print(a.count(1))
+a.append(4)
+print(a)
+a.insert(3,"你好")
+print(a)
+print(a.pop(3))
+print(a.remove(833))
